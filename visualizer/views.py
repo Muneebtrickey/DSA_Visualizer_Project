5a48c65c002111e5f0a77998af0566cbfd9c1d7e
Muneebtrickey <itsmuneeb566@gmail.com>
2026-03-31 23:08:43 +0500

    Quick and merge sort are added

--- visualizer/views.py
+++ visualizer/views.py
@@ -18,8 +18,70 @@ class SortingView(APIView):
             steps = self.selection_sort(arr.copy())
         elif algorithm == 'insertion':
             steps = self.insertion_sort(arr.copy())
+        elif algorithm == 'quick':
+            temp_arr = arr.copy()
+            self.quick_sort(temp_arr, 0, len(temp_arr) - 1, steps)
+        elif algorithm == 'merge':
+            temp_arr = arr.copy()
+            self.merge_sort(temp_arr, 0, len(temp_arr) - 1, steps)
         
         return Response({"steps": steps})
+
+    # --- Quick Sort ---
+    def quick_sort(self, arr, low, high, steps):
+        if low < high:
+            pivot_index = self.partition(arr, low, high, steps)
+            self.quick_sort(arr, low, pivot_index - 1, steps)
+            self.quick_sort(arr, pivot_index + 1, high, steps)
+
+    def partition(self, arr, low, high, steps):
+        pivot = arr[high]
+        i = low - 1
+        for j in range(low, high):
+            steps.append({'array': arr.copy(), 'comparing': [j, high]})
+            if arr[j] < pivot:
+                i += 1
+                arr[i], arr[j] = arr[j], arr[i]
+                steps.append({'array': arr.copy(), 'swapping': [i, j]})
+        arr[i + 1], arr[high] = arr[high], arr[i + 1]
+        steps.append({'array': arr.copy(), 'swapping': [i + 1, high]})
+        return i + 1
+
+    # --- Merge Sort ---
+    def merge_sort(self, arr, l, r, steps):
+        if l < r:
+            m = l + (r - l) // 2
+            self.merge_sort(arr, l, m, steps)
+            self.merge_sort(arr, m + 1, r, steps)
+            self.merge(arr, l, m, r, steps)
+
+    def merge(self, arr, l, m, r, steps):
+        n1 = m - l + 1
+        n2 = r - m
+        L = arr[l:m+1]
+        R = arr[m+1:r+1]
+        i = j = 0
+        k = l
+        while i < n1 and j < n2:
+            steps.append({'array': arr.copy(), 'comparing': [l + i, m + 1 + j]})
+            if L[i] <= R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            steps.append({'array': arr.copy(), 'swapping': [k]})
+            k += 1
+        while i < n1:
+            arr[k] = L[i]
+            i += 1
+            k += 1
+            steps.append({'array': arr.copy(), 'swapping': [k-1]})
+        while j < n2:
+            arr[k] = R[j]
+            j += 1
+            k += 1
+            steps.append({'array': arr.copy(), 'swapping': [k-1]})
 
     def bubble_sort(self, arr):
         steps = []
